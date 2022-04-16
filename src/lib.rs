7a6700854051a5782c3dad186f9b98a129b2999e
Yiliang He <yiliang.he@qq.com>
2022-04-16 09:33:52 +0800

    fix: load formula and add tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 extern crate logisheets_controller;
+extern crate logisheets_workbook;
+
 pub use logisheets_controller::{
     lex_success, Comment, MergeCell, SerdeErr, Style, Value, Workbook, Worksheet,
 };
+
+pub use logisheets_workbook::prelude::{simple_types::*, CtBorder, CtFont, CtUnderlineProperty};
