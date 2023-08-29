fda13d218d69f813ec89ba5a3305e322e2e1a93a
ImJeremyHe <yiliang.he@qq.com>
2023-08-29 14:02:54 +0800

    feat(funcs): attempt to add BINOM.INV

--- crates/controller/src/calc_engine/calculator/funcs/distribution/binom.rs
+++ crates/controller/src/calc_engine/calculator/funcs/distribution/binom.rs
@@ -42,3 +42,43 @@ where
         ConditionResult::Error(e) => CalcVertex::from_error(e),
     }
 }
+
+pub fn calc_inv<C>(args: Vec<CalcVertex>, fetcher: &mut C) -> CalcVertex
+where
+    C: Connector,
+{
+    assert_or_return!(args.len() == 3, ast::Error::Unspecified);
+    let mut args_iter = args.into_iter();
+
+    let first = fetcher.get_calc_value(args_iter.next().unwrap());
+    assert_f64_from_calc_value!(trials, first);
+    assert_or_return!(trials >= 0., ast::Error::Num);
+
+    let second = fetcher.get_calc_value(args_iter.next().unwrap());
+    assert_f64_from_calc_value!(probability, second);
+    assert_or_return!(probability >= 0. && probability <= 1., ast::Error::Num);
+
+    let third = fetcher.get_calc_value(args_iter.next().unwrap());
+    assert_f64_from_calc_value!(alpha, third);
+    assert_or_return!(alpha > 0. && alpha < 1., ast::Error::Num);
+
+    let trials = trials as u64;
+
+    let n = Binomial::new(probability, trials).unwrap();
+    let result = n.inverse_cdf(alpha);
+    CalcVertex::from_number(result as f64)
+}
+
+#[cfg(test)]
+mod tests {
+    use statrs::distribution::{Binomial, DiscreteCDF};
+
+    // A bug from statrs described here:
+    // https://github.com/statrs-dev/statrs/issues/185
+    #[ignore]
+    #[test]
+    fn binomial() {
+        let result = Binomial::new(0.5, 6).unwrap().inverse_cdf(0.75);
+        assert_eq!(result, 4);
+    }
+}
