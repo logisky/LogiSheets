69848fcebedfe55567d959043b28e156f9e730d1
ImJeremyHe <yiliang.he@qq.com>
2023-08-31 20:59:28 +0800

    feat(api): add remove_block api

--- crates/buildtools/src/generate.rs
+++ crates/buildtools/src/generate.rs
@@ -1,12 +1,9 @@
-use std::env;
-
 use gents::FileGroup;
 use logisheets_controller::controller::display::{DisplayRequest, DisplayResponse};
 use logisheets_controller::controller::edit_action::{ActionEffect, EditAction};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let path = args.last().unwrap();
+    let path = "src/bindings";
     let mut file_group = FileGroup::new();
     file_group.add::<DisplayRequest>();
     file_group.add::<DisplayResponse>();
