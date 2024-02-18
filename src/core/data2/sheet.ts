cbdbafb6de414abee49b18c630bfe3bdd38b9a3c
ImJeremyHe <yiliang.he@qq.com>
2024-02-18 10:14:04 +0800

    WIP

--- src/core/data2/sheet.ts
+++ src/core/data2/sheet.ts
@@ -1,7 +1,14 @@
-import {injectable} from 'inversify'
+import {inject, injectable} from 'inversify'
 import {getID} from '../ioc/id'
+import {RenderDataProvider} from './render'
+import {TYPES} from '../ioc/types'
+import {WorkbookService} from './workbook'
 
 @injectable()
 export class SheetService {
     readonly id = getID()
+    constructor(
+        @inject(TYPES.Render) private _render: RenderDataProvider,
+        @inject(TYPES.Data) private _workbook: WorkbookService
+    ) {}
 }
