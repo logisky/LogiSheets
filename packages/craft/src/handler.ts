7056583e343c123f7d855561115f56f4c128bb3b
Jeremy <yiliang.he@qq.com>
2025-04-20 20:47:37 +0800

    Enhance craft agent

--- packages/craft/src/handler.ts
+++ packages/craft/src/handler.ts
@@ -3,4 +3,11 @@ import {CraftId, CraftState} from './types'
 
 export interface CraftHandler extends WorkbookClient {
     getCraftState(craftId: CraftId): Promise<CraftState>
+    getAllKeys(craftId: CraftId): Promise<string[]>
+    getAllFields(craftId: CraftId): Promise<string[]>
+    getCoordinate(
+        craftId: CraftId,
+        key: string,
+        value: string
+    ): Promise<{row: number; col: number}>
 }
