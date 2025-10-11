6c4758f142973e592d1a9d026479291c38e1d45e
Jeremy <yiliang.he@qq.com>
2025-10-11 22:46:54 +0800

    Refactor canvas scrollbar

--- src/components/scrollbar/scroll_event.ts
+++
@@ -1,7 +0,0 @@
-import {ScrollbarType} from './scrollbar'
-export class ScrollEvent {
-    public type: ScrollbarType = 'x'
-    public delta = 0
-    public scrollDistance = 0
-    public trust = false
-}
