03269e7bc9296aab26d383e5859070dfbb30d4a6
Jeremy <yiliang.he@qq.com>
2025-11-10 09:58:50 +0800

    Remove top-bar

--- src/components/top-bar/content/start-item.ts
+++
@@ -1,8 +0,0 @@
-import {ItemType} from './start-item-type'
-export interface StartItem {
-    readonly type: ItemType
-    readonly value: unknown
-    readonly name: string
-    readonly opened: boolean
-    setOpened(opened: boolean): void
-}
