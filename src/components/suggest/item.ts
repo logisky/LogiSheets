7c309c3086732aa257fe7b76ec013799117d747c
Jeremy <yiliang.he@qq.com>
2025-12-29 10:07:48 +0800

    Avoid suggesting when trigger text is number

--- src/components/suggest/item.ts
+++ src/components/suggest/item.ts
@@ -3,12 +3,9 @@ import {lowerCase} from '@/core/strings'
 export class Candidate {
     constructor(
         public readonly triggerText: string,
-        // 函数开始括号的位置,用于在suggest的时候把光标设到该位置
         public readonly quoteStart?: number,
-        // 选择当前candidate之后，需要替换的文本
         public readonly plainText = ''
     ) {}
-    // 无选择功能，仅做文本显示
     public textOnly = false
     public desc = ''
     public spans: readonly SpanItem[] = []
